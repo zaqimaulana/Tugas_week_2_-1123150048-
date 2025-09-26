63f6fe0b95b89a397eb45c471e495cb1c578f96d
zaqimaulana <zaqimaulana72@gmail.com>
2025-09-27 01:17:44 +0700

    kalkulator sederhana

---
+++ bin/tugas_week_2_1123150048.dart
@@ -0,0 +1,5 @@
+import 'package:tugas_week_2_1123150048/tugas_week_2_1123150048.dart' as tugas_week_2_1123150048;
+
+void main(List<String> arguments) {
+  print('Hello world: ${tugas_week_2_1123150048.calculate()}!');
+}
---
+++ test/tugas_week_2_1123150048_test.dart
@@ -0,0 +1,8 @@
+import 'package:tugas_week_2_1123150048/tugas_week_2_1123150048.dart';
+import 'package:test/test.dart';
+
+void main() {
+  test('calculate', () {
+    expect(calculate(), 42);
+  });
+}
